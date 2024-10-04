4f1216633fa7e2527989ca275e581e01f9ab6d0b
Alicia Custodia <aliciagarciayuen@gmail.com>
2024-10-04 12:52:02 +0200

    Fixing

--- relecloud/models.py
+++ relecloud/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
-# Create your models here.
+# MODELOS
+
 class Destination(models.Model):
     name = models.CharField(
         unique=True,
@@ -19,6 +23,7 @@ class Destination(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
 
 class Cruise(models.Model):  
     name = models.CharField(
@@ -39,7 +44,8 @@ class Cruise(models.Model):
 
     def __str__(self) -> str:
         return self.name
-    
+
+
 class InfoRequest(models.Model):
     name = models.CharField(
         max_length=50,
@@ -56,3 +62,40 @@ class InfoRequest(models.Model):
         Cruise,
         on_delete=models.PROTECT
     )
+
+# FORMULARIOS
+
+class InfoRequestForm(forms.ModelForm):
+    class Meta:
+        model = InfoRequest
+        fields = ['name', 'email', 'cruise', 'notes']
+
+    def __init__(self, *args, **kwargs):
+        super(InfoRequestForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Submit'))
+
+
+class DestinationForm(forms.ModelForm):
+    class Meta:
+        model = Destination
+        fields = ['name', 'description', 'slug']
+
+    def __init__(self, *args, **kwargs):
+        super(DestinationForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Save Destination'))
+
+
+class CruiseForm(forms.ModelForm):
+    class Meta:
+        model = Cruise
+        fields = ['name', 'description', 'destinations']
+
+    def __init__(self, *args, **kwargs):
+        super(CruiseForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Save Cruise'))
